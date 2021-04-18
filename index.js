e0428546dcd1361a7d868857f0175eb61de18fcb
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-04-17 22:46:30 -0400

    added inquirer prompt to add members thro command line

--- index.js
+++ index.js
@@ -7,3 +7,93 @@ const Manager = require("./lib/Manager");
 
 let newTeamMembers = [];
 
+function init() {
+    startHtml();
+    getMemberInfo();
+}
+
+// prompt for user about team members
+function getMemberInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your team member's name?",
+            },
+            {
+                type: "list",
+                name: "role",
+                message: "What is your team member's role?"
+                choices: ["Engineer", "Intern", "Manager"],
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your team member's email address?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your team member's ID?"
+            },
+        ])
+
+        .then(function ({
+            name,
+            role,
+            id,
+            email
+        }) {
+            let roleInfo;
+            if (role === "Engineer") {
+                roleInfo = "Github Username";
+            } 
+            else if (role === "Intern") {
+                roleInfo = "School Name";
+            }
+            else {
+                roleInfo = "Office Number"
+            }
+
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        name: "roleInfo",
+                        message: `Enter team member's ${roleInfo}`,
+                    },
+                    {
+                        type: "list",
+                        name: "moreMembers",
+                        message: "Would you like to add more members to the team?",
+                        choices: ["yes", "no"],
+                    },
+                ])
+                .then(function ({
+                    roleInfo,
+                    moreMembers,
+                }) {
+                    let newMember;
+                    if (role === "Engineer") {
+                        newMember = new Engineer(name, id, email, roleInfo);
+                    }
+                    else if (role === "Intern") {
+                        newMember = new Intern(name, id, email, roleInfo);
+                    }
+                    else {
+                        newMember = new Manager(name, id, email, roleInfo);
+                    }
+                    newTeamMembers.push(newMember);
+                    addHtml(newMember).then(function () {
+                        if (moreMembers === "yes") {
+                            getMemberInfo();
+                        }
+                        else {
+                            finishHtml();
+                        }
+                    });
+                    
+                });
+        });
+}
